293ea8803616c5ec573032b48b1366314969d5f3
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2021-03-10 07:44:34 +0530

    Add readme images

--- src/redux/actions/library.js
+++ src/redux/actions/library.js
@@ -2,8 +2,7 @@ import { GET_LIBRARY_NOVELS } from "./types";
 import { getLibraryFromDb } from "../../services/db";
 
 export const getLibraryNovels = () => async (dispatch) => {
-    console.log("x");
-    // getLibraryFromDb().then((res) => console.log(res));
+    getLibraryFromDb().then((res) => console.log(res));
 
     dispatch({
         type: GET_LIBRARY_NOVELS,
