d0b11c9b474d16f4aace29a4cdbaf5e44b20f448
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2021-06-19 09:17:52 +0530

    Rename folders

--- src/Hooks/showToast.js
+++
@@ -1,5 +0,0 @@
-import { ToastAndroid } from "react-native";
-
-export const showToast = (message) => {
-    ToastAndroid.show(message, ToastAndroid.SHORT);
-};
--- src/redux/updates/updates.types.js
+++
@@ -1,2 +0,0 @@
-export const GET_UPDATES = "GET_UPDATES";
-export const LOAD_UPDATES = "LOAD_UPDATES";
