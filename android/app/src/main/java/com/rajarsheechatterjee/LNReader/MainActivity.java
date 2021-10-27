7375eb7ad00aead369dcf8e4a00ebbc7b0b06f20
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2021-10-27 06:48:46 +0530

    Fix splitscreen view crash (Closes #77)

--- android/app/src/main/java/com/rajarsheechatterjee/LNReader/MainActivity.java
+++ android/app/src/main/java/com/rajarsheechatterjee/LNReader/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends ReactActivity {
       w.setNavigationBarColor(Color.TRANSPARENT);
       w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
-      super.onCreate(savedInstanceState);
+      super.onCreate(null);
       SplashScreen.show(this, SplashScreenImageResizeMode.COVER, ReactRootView.class, false);
 
     }
