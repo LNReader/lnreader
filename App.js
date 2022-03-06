36b9e2cf698bb9cac4290b6592e986882b571585
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2022-03-06 09:27:27 +0530

    style: run prettier

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
-import {enableFreeze} from 'react-native-screens';
+import { enableFreeze } from 'react-native-screens';
 
 enableFreeze(true);
 
-import React, {useEffect} from 'react';
-import {StatusBar} from 'react-native';
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native';
 
-import {useFonts} from 'expo-font';
-import {Provider as PaperProvider} from 'react-native-paper';
-import {Provider} from 'react-redux';
-import {persistor, store} from './src/redux/store';
-import {PersistGate} from 'redux-persist/lib/integration/react';
+import { useFonts } from 'expo-font';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider } from 'react-redux';
+import { persistor, store } from './src/redux/store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import * as SplashScreen from 'expo-splash-screen';
 
 import Main from './src/navigators/Main';
 
-import {createDB} from './src/database/DBHelper';
-import {fonts} from './src/theme/fonts';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { createDB } from './src/database/DBHelper';
+import { fonts } from './src/theme/fonts';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   const [loaded] = useFonts(fonts);
