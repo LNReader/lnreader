3dbb835045f91c67decedabfbb16673621af7c60
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2022-04-10 21:44:40 +0530

    chore: Add Strings to Translations

--- strings/translations.ts
+++ strings/translations.ts
@@ -4,11 +4,13 @@ import i18n from 'i18n-js';
 import en from './languages/en/strings.json';
 import es from './languages/es/strings.json';
 import tr from './languages/tr/strings.json';
+import ru from './languages/ru/strings.json';
+import ar from './languages/ar/strings.json';
 
 import { StringMap } from './types';
 
 i18n.fallbacks = true;
-i18n.translations = { en, es, tr };
+i18n.translations = { en, es, tr, ru, ar };
 i18n.locale = Localization.locale;
 
 export const getString = (stringKey: keyof StringMap) => i18n.t(stringKey);
