3fbca6c14d0d45282791f42289e0f5e41775d107
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2022-05-04 06:56:30 +0530

    fix: Links not working in Render HTML (Closes #245)

--- src/screens/reader/utils/sanitizeChapterText.ts
+++ src/screens/reader/utils/sanitizeChapterText.ts
@@ -3,7 +3,7 @@ import sanitizeHtml from 'sanitize-html';
 export const sanitizeChapterText = (html: string) => {
   const text = sanitizeHtml(html, {
     allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-    allowedAttributes: { 'img': ['src'] },
+    allowedAttributes: { 'img': ['src'], 'a': ['href'] },
     allowedSchemes: ['data', 'http', 'https'],
   });
 
