ccdb150d6ae0a947f6b229419e49026e3f98dccf
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2022-05-06 20:12:23 +0530

    fix: Add Strings to Translations

--- strings/translations.ts
+++ strings/translations.ts
@@ -7,11 +7,12 @@ import tr from './languages/tr/strings.json';
 import ru from './languages/ru/strings.json';
 import ar from './languages/ar/strings.json';
 import uk from './languages/uk/strings.json';
+import pt from './languages/pt/strings.json';
 
 import { StringMap } from './types';
 
 i18n.fallbacks = true;
-i18n.translations = { en, es, tr, ru, ar, uk };
+i18n.translations = { en, es, tr, ru, ar, uk, pt };
 i18n.locale = Localization.locale;
 
 export const getString = (stringKey: keyof StringMap) => i18n.t(stringKey);
