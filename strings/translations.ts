a1f769cde68a5954209cef6deb25240c901b562d
Rider21 <58046032+Rider21@users.noreply.github.com>
2022-08-01 17:55:40 +0300

    fix

--- strings/translations.ts
+++ strings/translations.ts
@@ -3,9 +3,11 @@ import * as dayjs from 'dayjs';
 import i18n from 'i18n-js';
 
 import customParseFormat from 'dayjs/plugin/customParseFormat';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import calendar from 'dayjs/plugin/calendar';
 dayjs.extend(customParseFormat);
+dayjs.extend(localizedFormat);
 dayjs.extend(relativeTime);
 dayjs.extend(calendar);
 
