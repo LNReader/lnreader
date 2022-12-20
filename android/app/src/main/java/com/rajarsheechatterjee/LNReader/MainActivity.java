f52e679c2d300384ee641d705fd12fb5f7f477c7
Patrick <patrick.los@proton.me>
2022-12-20 22:05:57 +0100

    fixed animation

--- android/app/src/main/java/com/rajarsheechatterjee/LNReader/MainActivity.java
+++ android/app/src/main/java/com/rajarsheechatterjee/LNReader/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends ReactActivity {
         w.setNavigationBarColor(Color.TRANSPARENT);
         w.getDecorView()
                 .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        SplashScreen.show(this,R.style.AppTheme, R.id.lottie);
+        SplashScreen.show(this,R.style.SplashScreenTheme, R.id.lottie);
         SplashScreen.setAnimationFinished(true);
         super.onCreate(null);
     }
