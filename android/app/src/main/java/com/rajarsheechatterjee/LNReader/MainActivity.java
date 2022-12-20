bddc8a562f70040ea8123d9530183e35a89d9d12
Patrick <patrick.los@proton.me>
2022-12-20 21:58:00 +0100

    fixed unstyled bars

--- android/app/src/main/java/com/rajarsheechatterjee/LNReader/MainActivity.java
+++ android/app/src/main/java/com/rajarsheechatterjee/LNReader/MainActivity.java
@@ -1,15 +1,14 @@
 package com.rajarsheechatterjee.LNReader;
 
-import android.content.res.Configuration;
 import android.content.Intent;
-
-import android.os.Bundle;
+import android.content.res.Configuration;
 import android.graphics.Color;
-import android.view.KeyEvent;
-import android.view.WindowManager;
 import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -46,7 +45,7 @@ public class MainActivity extends ReactActivity {
         w.setNavigationBarColor(Color.TRANSPARENT);
         w.getDecorView()
                 .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        SplashScreen.show(this, R.id.lottie);
+        SplashScreen.show(this,R.style.AppTheme, R.id.lottie);
         SplashScreen.setAnimationFinished(true);
         super.onCreate(null);
     }
