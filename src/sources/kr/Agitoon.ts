a6d21cbc27a8e9a286bc3aeb862ad0ec5bfe0176
Vigneshwar M <vigneshwarm@Vigneshwars-MacBook-Pro.local>
2023-02-14 12:00:52 +0530

    add: kr sources

---
+++ src/sources/kr/Agitoon.ts
@@ -0,0 +1,32 @@
+import { fetchHtml } from '@utils/fetch/fetch';
+import * as cheerio from 'cheerio';
+import { SourceChapter, SourceNovel, SourceNovelItem } from '../types';
+
+const sourceId = 145;
+const sourceName = 'Agitoon';
+const baseUrl = 'https://agit501.xyz/';
+
+const popularNovels = () => {
+  return null;
+};
+
+const parseNovelAndChapters = () => {
+  return null;
+};
+
+const parseChapter = () => {
+  return null;
+};
+
+const searchNovels = () => {
+  return null;
+};
+
+const AgitoonScraper = {
+  popularNovels,
+  parseNovelAndChapters,
+  parseChapter,
+  searchNovels,
+};
+
+export default AgitoonScraper;
