63909bf52e02cf08626f3d87c8139ceff01c0301
K1ngfish3r <trulyihu@gmail.com>
2023-02-16 01:48:58 +0800

    fix(source): Update WPMangaStreamScraper's Cover Selector (#543)
    * fixes images on noblemtl
    
    * fix images on noblemtl
    
    * fix ESLint errors

--- src/sources/multisrc/wpmangastream/WPMangaStreamScraper.js
+++ src/sources/multisrc/wpmangastream/WPMangaStreamScraper.js
@@ -24,7 +24,7 @@ class WPMangaStreamScraper {
     loadedCheerio('article.bs').each(function () {
       const novelName = loadedCheerio(this).find('.ntitle').text().trim();
       let image = loadedCheerio(this).find('img');
-      const novelCover = image.attr('src');
+      const novelCover = image.attr('data-src') || image.attr('src');
 
       const novelUrl = loadedCheerio(this).find('a').attr('href');
 
@@ -60,7 +60,9 @@ class WPMangaStreamScraper {
 
     novel.novelName = loadedCheerio('.entry-title').text();
 
-    novel.novelCover = loadedCheerio('img.wp-post-image').attr('src');
+    novel.novelCover =
+      loadedCheerio('img.wp-post-image').attr('data-src') ||
+      loadedCheerio('img.wp-post-image').attr('src');
 
     loadedCheerio('div.spe > span').each(function () {
       const detailName = loadedCheerio(this).find('b').text().trim();
