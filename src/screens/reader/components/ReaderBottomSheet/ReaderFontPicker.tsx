295368b4c0788ad3ce5ee220edcf796a7cf359e0
Patrick <69157453+CD-Z@users.noreply.github.com>
2023-02-16 22:10:02 +0100

    readersettingsscreen + fix font style selection in Bottomsheet

--- src/screens/reader/components/ReaderBottomSheet/ReaderFontPicker.tsx
+++ src/screens/reader/components/ReaderBottomSheet/ReaderFontPicker.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import color from 'color';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -9,6 +9,7 @@ import { useTheme } from '@hooks/useTheme';
 
 import { setReaderSettings } from '../../../../redux/settings/settings.actions';
 import { Font, readerFonts } from '../../../../utils/constants/readerConstants';
+import { FlatList } from 'react-native-gesture-handler';
 
 const ReaderFontPicker = () => {
   const dispatch = useAppDispatch();
@@ -23,61 +24,66 @@ const ReaderFontPicker = () => {
       <Text style={[{ color: theme.textColorSecondary }, styles.title]}>
         {getString('readerScreen.bottomSheet.fontStyle')}
       </Text>
-      <ScrollView
+      <FlatList
+        data={readerFonts}
+        renderItem={FontChipItem}
         horizontal={true}
         showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.scrollContainer}
-      >
-        {readerFonts.map(item => (
-          <View
-            key={item.fontFamily}
-            style={[
-              styles.container,
-              {
-                backgroundColor: theme.surfaceVariant,
-              },
-            ]}
-          >
-            <Pressable
-              style={styles.content}
-              onPress={() =>
-                dispatch(setReaderSettings('fontFamily', item.fontFamily))
-              }
-              android_ripple={{
-                color: color(theme.primary).alpha(0.12).string(),
-              }}
-            >
-              {isSelected(item) ? (
-                <MaterialCommunityIcons
-                  name="check"
-                  color={theme.primary}
-                  size={16}
-                />
-              ) : null}
-              <Text
-                style={[
-                  styles.label,
-                  {
-                    fontFamily: item.fontFamily,
-                    color: isSelected(item)
-                      ? theme.primary
-                      : theme.textColorPrimary,
-                  },
-                  isSelected(item) && styles.mLeft,
-                ]}
-              >
-                {item.name}
-              </Text>
-            </Pressable>
-          </View>
-        ))}
-      </ScrollView>
+      />
     </View>
   );
+  interface FontChipProps {
+    item: Font;
+  }
+
+  function FontChipItem({ item }: FontChipProps) {
+    return (
+      <View
+        key={item.fontFamily}
+        style={[
+          styles.container,
+          {
+            backgroundColor: theme.surfaceVariant,
+          },
+        ]}
+      >
+        <Pressable
+          style={styles.content}
+          onPress={() =>
+            dispatch(setReaderSettings('fontFamily', item.fontFamily))
+          }
+          android_ripple={{
+            color: color(theme.primary).alpha(0.12).string(),
+          }}
+        >
+          {isSelected(item) ? (
+            <MaterialCommunityIcons
+              name="check"
+              color={theme.primary}
+              size={16}
+            />
+          ) : null}
+          <Text
+            style={[
+              styles.label,
+              {
+                fontFamily: item.fontFamily,
+                color: isSelected(item)
+                  ? theme.primary
+                  : theme.textColorPrimary,
+              },
+              isSelected(item) && styles.mLeft,
+            ]}
+          >
+            {item.name}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
 };
 
 export default ReaderFontPicker;
-
 const styles = StyleSheet.create({
   container: {
     borderRadius: 8,
@@ -85,12 +91,13 @@ const styles = StyleSheet.create({
     marginHorizontal: 6,
   },
   content: {
+    flexGrow: 1,
     flexDirection: 'row',
-    alignItems: 'center',
-    borderRadius: 8,
-    paddingVertical: 4,
+    paddingVertical: 1,
+    justifyContent: 'center',
     paddingHorizontal: 10,
     minHeight: 32,
+    alignItems: 'center',
   },
   row: {
     flexDirection: 'row',
@@ -101,9 +108,6 @@ const styles = StyleSheet.create({
   },
   title: {
     marginRight: 16,
-  },
-  scrollContainer: {
-    alignItems: 'center',
   },
   label: {
     fontSize: 16,
