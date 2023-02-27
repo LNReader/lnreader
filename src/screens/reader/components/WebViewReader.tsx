6e501c3c232baaf6e71690ae9b5687e353cbbd32
Nyagami <hoangquan05112002@gmail.com>
2023-02-27 14:21:48 +0700

    update: swipe gestures: change chapter on webview

--- src/screens/reader/components/WebViewReader.tsx
+++ src/screens/reader/components/WebViewReader.tsx
@@ -325,28 +325,19 @@ const WebViewReader: React.FC<WebViewReaderProps> = props => {
                       swipeGestures
                         ? `
                         <script>
-                          var initialX = null;
-                          var initialY = null;
+                          let initialX = null;
+                          let initialY = null;
                           document.addEventListener("touchstart", e => {
                             initialX = e.changedTouches[0].screenX;
                             initialY = e.changedTouches[0].screenY;
                           });
                           document.addEventListener("touchend", e => {
-                            if (initialX === null || initialY === null) return; 
-                            var currentX = e.changedTouches[0].screenX;
-                            var currentY = e.changedTouches[0].screenY;
-                            var diffX = currentX - initialX;
-                            var diffY = currentY - initialY;
+                            let diffX = e.changedTouches[0].screenX - initialX;
+                            let diffY = e.changedTouches[0].screenY - initialY;
                             if (Math.abs(diffX) > Math.abs(diffY) && Math.abs(diffX) > 10) {
                               e.preventDefault();
-                              if (diffX < 0) {
-                                  window.ReactNativeWebView.postMessage(JSON.stringify({type:"next"}))
-                              } else {
-                                  window.ReactNativeWebView.postMessage(JSON.stringify({type:"prev"}))
-                              }  
+                              window.ReactNativeWebView.postMessage(JSON.stringify({type: diffX<0 ? "next" : "prev"}))
                             }
-                            initialX = null;
-                            initialY = null;
                           });
                           </script>`
                         : ''
