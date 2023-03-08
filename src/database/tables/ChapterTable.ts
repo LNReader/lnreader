f04b27ec97d267b4ae0ea940b331e988f3f4ef3a
Nyagami <hoangquan05112002@gmail.com>
2023-03-09 01:25:13 +0700

    update db

---
+++ src/database/tables/ChapterTable.ts
@@ -0,0 +1,13 @@
+export const createChapterTableQuery = `
+    CREATE TABLE IF NOT EXISTS Chapter (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        novel_id INTEGER NOT NULL,
+        url TEXT NOT NULL,
+        name TEXT NOT NULL,
+        release TEXT,
+        bookmark INTEGER DEFAULT 0, 
+        unread INTEGER DEFAULT 1, 
+        is_downloaded INTEGER DEFAULT 0,
+        FOREIGN KEY (novel_id) REFERENCES Novel(id) ON DELETE CASCADE
+    )
+`;
