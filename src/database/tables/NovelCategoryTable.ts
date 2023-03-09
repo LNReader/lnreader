3ec5f02c21a6e9625481d011ee3e87b9c3a790c2
Nyagami <hoangquan05112002@gmail.com>
2023-03-09 10:28:18 +0700

    update constrait

--- src/database/tables/NovelCategoryTable.ts
+++ src/database/tables/NovelCategoryTable.ts
@@ -3,6 +3,7 @@ export const createNovelCategoryTableQuery = `
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     novel_id INTEGER NOT NULL,
     category_id INTEGER NOT NULL,
+    UNIQUE(novel_id, category_id),
     FOREIGN KEY (novel_id) REFERENCES Novel(id),
     FOREIGN KEY (category_id) REFERENCES Category(id) ON DELETE CASCADE
   )
