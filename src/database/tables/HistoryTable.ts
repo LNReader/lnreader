2544ed72bf6c190d4175aea2384f189d66870b16
Nyagami <hoangquan05112002@gmail.com>
2023-03-09 09:26:56 +0700

    format table name

--- src/database/tables/HistoryTable.ts
+++ src/database/tables/HistoryTable.ts
@@ -1,5 +1,5 @@
 export const createHistoryTableQuery = `
-    CREATE TABLE IF NOT EXISTS history(
+    CREATE TABLE IF NOT EXISTS History(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         chapter_id INTEGER NOT NULL UNIQUE,
         read_time TEXT DEFAULT CURRENT_TIMESTAMP,
