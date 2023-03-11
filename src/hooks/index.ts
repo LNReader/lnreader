626b1a5140adb1a51954951b858c2db697d48fa5
Nyagami <hoangquan05112002@gmail.com>
2023-03-11 23:02:17 +0700

    remove never use hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,7 +3,6 @@ export { default as useHistory } from './useHistory';
 export { default as useUpdates } from './useUpdates';
 export { default as useFullscreenMode } from './useFullscreenMode';
 export { default as useChapter } from './useChapter';
-export { default as useNextAndPrevChapter } from './useNextAndPrevChapter';
 export { default as useNovelTrackerInfo } from './useNovelTrackerInfo';
 export { default as useBoolean } from './useBoolean';
 export { useTheme } from './useTheme';
