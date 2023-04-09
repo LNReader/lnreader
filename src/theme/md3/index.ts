d743276cc3a1b16ef2f649fe191acb1cc1eeaed4
CD-Z <p_loser_Austria@gmx.at>
2023-04-09 22:08:22 +0200

    fixed midnightdusk

--- src/theme/md3/index.ts
+++ src/theme/md3/index.ts
@@ -1,5 +1,5 @@
 export { defaultTheme } from './defaultTheme';
-export { midnightDusk } from './mignightDusk';
+export { midnightDusk } from './midnightDusk';
 export { tealTurquoise } from './tealTurquoise';
 export { yotsubaTheme } from './yotsuba';
 export { lavenderTheme } from './lavender';
