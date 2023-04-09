9f393f013e499a0733a434e20d441ee83eebc2d3
CD-Z <p_loser_Austria@gmx.at>
2023-04-09 15:30:57 +0200

    Fixed ReaderLineHeight and ReaderTextSize

--- src/screens/settings/SettingsReaderScreen/ReaderTextSize.tsx
+++ src/screens/settings/SettingsReaderScreen/ReaderTextSize.tsx
@@ -8,24 +8,20 @@ import Counter from '@components/Counter/Counter';
 
 interface ReaderTextSizeProps {
   labelStyle?: TextStyle | TextStyle[];
-  onFocus?: (event: Event) => void;
 }
 
-const ReaderTextSize: React.FC<ReaderTextSizeProps> = ({
-  labelStyle,
-  onFocus,
-}) => {
+const ReaderTextSize: React.FC<ReaderTextSizeProps> = ({ labelStyle }) => {
   const dispatch = useAppDispatch();
 
   const { textSize } = useReaderSettings();
   return (
     <Counter
-      onFocus={onFocus}
       labelStyle={labelStyle}
       label={getString('readerScreen.bottomSheet.textSize')}
       value={textSize}
       onChange={val => dispatch(setReaderSettings('textSize', val))}
-      min={0}
+      minimumValue={0}
+      maximumValue={100}
     />
   );
 };
