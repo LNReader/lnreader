8414684bac344c73b8b30d2ee9cb22970aaa4b1a
Nyagami <hoangquan05112002@gmail.com>
2023-04-11 11:01:29 +0700

    shorter langue native name

--- src/utils/constants/languages.ts
+++ src/utils/constants/languages.ts
@@ -2,12 +2,12 @@
 
 export enum Languages {
   Arabic = 'العربية',
-  Chinese = '中文 (Zhōngwén), 汉语, 漢語',
+  Chinese = '中文, 汉语, 漢語',
   English = 'English',
-  French = 'français, langue française',
+  French = 'français, française',
   Indonesian = 'Bahasa Indonesia',
-  Japanese = '日本語 (にほんご／にっぽんご)',
-  Korean = '한국어 (韓國語), 조선말 (朝鮮語)',
+  Japanese = '日本語',
+  Korean = '한국어, 조선말',
   Portuguese = 'Português',
   Russian = 'русский язык',
   Spanish = 'español, castellano',
