3b2ee3d78b232688f49b80ac8898b0f362d46e81
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2023-09-02 11:06:07 +0530

    refactor: Custom CSS/JS Modal (#705)
    * refactor: Custom CSS/JS Modal
    
    * Close Modal on Selecting a File

--- src/screens/settings/SettingsReaderScreen/Modals/CustomFileModal.tsx
+++ src/screens/settings/SettingsReaderScreen/Modals/CustomFileModal.tsx
@@ -1,118 +1,101 @@
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Modal, overlay, TextInput } from 'react-native-paper';
-import { Button } from '@components/index';
-import { MD3ThemeType } from '@theme/types';
-import { getString } from '@strings/translations';
-import { useAppDispatch } from '@redux/hooks';
-import * as DocumentPicker from 'expo-document-picker';
 import { StorageAccessFramework } from 'expo-file-system';
-import { showToast } from '@hooks/showToast';
-import { setReaderSettings } from '@redux/settings/settings.actions';
+import * as DocumentPicker from 'expo-document-picker';
 
-interface CustomCSSModalProps {
+import { Button } from '@components/index';
+
+import { showToast } from '@hooks/showToast';
+import { useTheme } from '@hooks/useTheme';
+import { getString } from '@strings/translations';
+
+interface CustomFileModal {
   visible: boolean;
   onDismiss: () => void;
-  customFile: string;
-  setCustomFile: (val: string) => void;
-  theme: MD3ThemeType;
+  defaultValue: string;
+  onSave: (val: string) => void;
   title: string;
-  type?: string;
-  openFileButtonLabel?: string;
+  mimeType: 'text/css' | 'application/javascript';
+  openFileLabel: string;
   placeholder?: string;
+  description?: string;
 }
 
-const CustomFileModal: React.FC<CustomCSSModalProps> = ({
+const CustomFileModal: React.FC<CustomFileModal> = ({
   onDismiss,
   visible,
-  customFile,
-  setCustomFile,
-  theme,
+  defaultValue,
+  onSave,
   title,
-  type = 'CSS',
-  openFileButtonLabel = getString(
-    'moreScreen.settingsScreen.readerSettings.openCSSFile',
-  ),
-  placeholder = '',
+  mimeType,
+  openFileLabel,
+  placeholder,
+  description,
 }) => {
-  const [text, setText] = useState(customFile);
-  const dispatch = useAppDispatch();
-  let mimeType: string;
-  if (type === 'CSS') {
-    mimeType = 'text/css';
-  } else {
-    mimeType = 'application/javascript';
-  }
+  const theme = useTheme();
+  const [text, setText] = useState('');
 
-  const openCSS = async () => {
+  const openDocumentPicker = async () => {
     try {
-      const rawCSS = await DocumentPicker.getDocumentAsync({
+      const file = await DocumentPicker.getDocumentAsync({
         copyToCacheDirectory: false,
         type: mimeType,
       });
-      if (rawCSS.type === 'success') {
-        let css = await StorageAccessFramework.readAsStringAsync(rawCSS.uri);
-        setText(css);
+
+      if (file.type === 'success') {
+        const content = await StorageAccessFramework.readAsStringAsync(
+          file.uri,
+        );
+
+        onSave(content.trim());
+        onDismiss();
       }
     } catch (error: any) {
       showToast(error.message);
     }
   };
-  const dismiss = () => {
-    setCustomFile(text.trim());
-    onDismiss();
-  };
 
   return (
     <Modal
       visible={visible}
-      onDismiss={dismiss}
+      onDismiss={onDismiss}
       contentContainerStyle={[
         styles.modalContainer,
         { backgroundColor: overlay(2, theme.surface) },
       ]}
     >
-      <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={180}>
-        <Text style={[styles.modalTitle, { color: theme.onSurface }]}>
-          {title}
-        </Text>
-        <Text style={[{ color: theme.onSurfaceVariant }]}>
-          {type === 'CSS'
-            ? getString('moreScreen.settingsScreen.readerSettings.cssHint')
-            : getString('moreScreen.settingsScreen.readerSettings.jsHint')}
-        </Text>
-        <TextInput
-          theme={{ colors: { ...theme } }}
-          underlineColor={theme.outline}
-          value={text}
-          onChangeText={setText}
-          mode="outlined"
-          placeholder={placeholder}
-          placeholderTextColor={theme.onSurfaceVariant}
-          multiline
-          style={[
-            { color: theme.onSurface },
-            styles.fontSizeL,
-            styles.customCSSContainer,
-          ]}
+      <Text style={[styles.modalTitle, { color: theme.onSurface }]}>
+        {title}
+      </Text>
+      <Text style={[{ color: theme.onSurfaceVariant }]}>{description}</Text>
+      <TextInput
+        multiline
+        mode="outlined"
+        defaultValue={defaultValue}
+        onChangeText={setText}
+        placeholder={placeholder}
+        placeholderTextColor={theme.onSurfaceVariant}
+        underlineColor={theme.outline}
+        style={[{ color: theme.onSurface }, styles.textInput]}
+        theme={{ colors: { ...theme } }}
+      />
+      <View style={styles.customCSSButtons}>
+        <Button
+          onPress={() => {
+            onSave(text.trim());
+            onDismiss();
+          }}
+          style={styles.button}
+          title={getString('common.save')}
+          mode="contained"
         />
-        <View style={styles.customCSSButtons}>
-          <Button
-            onPress={() => {
-              dispatch(setReaderSettings(`custom${type}`, text.trim()));
-              dismiss();
-            }}
-            style={styles.button}
-            title={getString('common.save')}
-            mode="contained"
-          />
-          <Button
-            style={styles.button}
-            onPress={openCSS}
-            title={openFileButtonLabel}
-          />
-        </View>
-      </KeyboardAvoidingView>
+        <Button
+          style={styles.button}
+          onPress={openDocumentPicker}
+          title={openFileLabel}
+        />
+      </View>
     </Modal>
   );
 };
@@ -125,14 +108,12 @@ const styles = StyleSheet.create({
     padding: 24,
     borderRadius: 28,
   },
-  fontSizeL: {
-    fontSize: 16,
-  },
-  customCSSContainer: {
+  textInput: {
     height: 220,
     borderRadius: 14,
     marginTop: 16,
     marginBottom: 8,
+    fontSize: 16,
   },
   modalTitle: {
     fontSize: 24,
