a19b374cb4882e5b368ac1b5e375f49ab6bc9da7
Nyagami <hoangquan05112002@gmail.com>
2024-01-13 16:25:53 +0700

    fix: small plugin card, loading when installing

--- src/screens/browse/components/PluginCard.tsx
+++ src/screens/browse/components/PluginCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 import { IconButtonV2 } from '@components';
@@ -10,6 +10,7 @@ import { PluginItem } from '@plugins/types';
 import { PluginMenu } from './PluginMenu';
 import FastImage from 'react-native-fast-image';
 import { showToast } from '@utils/showToast';
+import { ActivityIndicator } from 'react-native-paper';
 
 interface Props {
   installed: boolean;
@@ -29,51 +30,61 @@ const PluginCard: React.FC<Props> = ({
   installPlugin,
   uninstallPlugin,
   updatePlugin,
-}) => (
-  <Pressable
-    style={styles.container}
-    onPress={() => installed && navigateToSource(plugin)}
-    android_ripple={{ color: theme.rippleColor }}
-  >
-    <View style={styles.flexRow}>
-      <FastImage source={{ uri: plugin.iconUrl }} style={styles.icon} />
-      <View style={styles.details}>
-        <Text style={{ color: theme.onSurface }}>{plugin.name}</Text>
-        <Text style={{ color: theme.onSurface, fontWeight: 'bold' }}>
-          {`ID: ${plugin.id}`}
-        </Text>
-        <Text style={[{ color: theme.onSurfaceVariant }]}>
-          {`${plugin.lang}\n${plugin.version}`}
-        </Text>
+}) => {
+  const [isInstalling, setIsInstalling] = useState(false);
+  return (
+    <Pressable
+      style={styles.container}
+      onPress={() => installed && navigateToSource(plugin)}
+      android_ripple={{ color: theme.rippleColor }}
+    >
+      <View style={styles.flexRow}>
+        <FastImage source={{ uri: plugin.iconUrl }} style={styles.icon} />
+        <View style={styles.details}>
+          <Text style={[{ color: theme.onSurface }, styles.name]}>
+            {plugin.name}
+          </Text>
+          <Text style={[{ color: theme.onSurfaceVariant }, styles.addition]}>
+            {`ID: ${plugin.id}`}
+          </Text>
+          <Text style={[{ color: theme.onSurfaceVariant }, styles.addition]}>
+            {`${plugin.lang} - ${plugin.version}`}
+          </Text>
+        </View>
       </View>
-    </View>
-    <View style={styles.flexRow}>
-      {installed ? (
-        <PluginMenu
-          plugin={plugin}
-          theme={theme}
-          navigateToSource={navigateToSource}
-          uninstallPlugin={uninstallPlugin}
-          updatePlugin={updatePlugin}
-        />
-      ) : (
-        <>
-          <IconButtonV2
-            name={'download'}
-            size={22}
-            color={theme.primary}
-            onPress={() =>
-              installPlugin(plugin).then(() =>
-                showToast(`Installed ${plugin.name}`),
-              )
-            }
+      <View style={styles.flexRow}>
+        {installed ? (
+          <PluginMenu
+            plugin={plugin}
             theme={theme}
+            navigateToSource={navigateToSource}
+            uninstallPlugin={uninstallPlugin}
+            updatePlugin={updatePlugin}
           />
-        </>
-      )}
-    </View>
-  </Pressable>
-);
+        ) : (
+          <>
+            {!isInstalling ? (
+              <IconButtonV2
+                name={'download'}
+                size={22}
+                color={theme.primary}
+                onPress={() => {
+                  setIsInstalling(true);
+                  installPlugin(plugin)
+                    .then(() => showToast(`Installed ${plugin.name}`))
+                    .finally(() => setIsInstalling(false));
+                }}
+                theme={theme}
+              />
+            ) : (
+              <ActivityIndicator color={theme.primary} size={22} />
+            )}
+          </>
+        )}
+      </View>
+    </Pressable>
+  );
+};
 
 export default PluginCard;
 
@@ -86,13 +97,21 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
   },
   icon: {
-    height: 45,
-    width: 45,
+    height: 40,
+    width: 40,
     borderRadius: 4,
     backgroundColor: coverPlaceholderColor,
   },
   details: {
     marginLeft: 16,
+  },
+  addition: {
+    textAlign: 'left',
+    fontSize: 12,
+    fontWeight: '300',
+  },
+  name: {
+    fontWeight: 'bold',
   },
   flexRow: {
     flexDirection: 'row',
