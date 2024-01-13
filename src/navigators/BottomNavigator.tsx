f8647a8e49afe8101aaf8beec584f84e495f1a7f
Nyagami <hoangquan05112002@gmail.com>
2024-01-14 02:12:12 +0700

    upgrade react-native-paper to fix modal ondismis

--- src/navigators/BottomNavigator.tsx
+++ src/navigators/BottomNavigator.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
 
 import Library from '../screens/library/LibraryScreen';
 import Updates from '../screens/updates/UpdatesScreen';
