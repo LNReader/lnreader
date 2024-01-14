2b99c3d17dc265347134cd94299f397e5ede1408
Nyagami <hoangquan05112002@gmail.com>
2024-01-14 23:22:48 +0700

    fix: android user-agent

--- src/utils/fetch/fetch.ts
+++ src/utils/fetch/fetch.ts
@@ -1,5 +1,5 @@
 export const defaultUserAgentString =
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36';
+  'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36';
 
 export const fetchApi = async (
   url: string,
