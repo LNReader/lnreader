8903651ac120307538482d32d7e93a4f5456e062
nyagami <hoangquan05112002@gmail.com>
2024-02-25 10:48:21 +0700

    fix: ScrollView for morescreen

--- src/screens/more/MoreScreen.tsx
+++ src/screens/more/MoreScreen.tsx
@@ -30,149 +30,147 @@ const MoreScreen = ({ navigation }: MoreStackScreenProps) => {
     setLibrarySettings({ incognitoMode: !incognitoMode });
 
   return (
-    <>
+    <ScrollView>
       <MoreHeader
         title={getString('more')}
         navigation={navigation}
         theme={theme}
       />
-      <ScrollView style={{ flex: 1 }}>
-        <List.Section>
-          <Pressable
-            android_ripple={{ color: theme.rippleColor }}
-            style={{
-              paddingHorizontal: 16,
-              paddingVertical: 14,
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}
-            onPress={enableDownloadedOnlyMode}
-          >
-            <View style={{ flexDirection: 'row' }}>
-              <List.Icon theme={theme} icon="cloud-off-outline" />
-              <View style={{ marginLeft: 16 }}>
-                <Text
-                  style={{
-                    color: theme.onSurface,
-                    fontSize: 16,
-                  }}
-                >
-                  {getString('moreScreen.downloadOnly')}
-                </Text>
-                <Text style={{ color: theme.onSurfaceVariant }}>
-                  {getString('moreScreen.downloadOnlyDesc')}
-                </Text>
-              </View>
+      <List.Section>
+        <Pressable
+          android_ripple={{ color: theme.rippleColor }}
+          style={{
+            paddingHorizontal: 16,
+            paddingVertical: 14,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+          onPress={enableDownloadedOnlyMode}
+        >
+          <View style={{ flexDirection: 'row' }}>
+            <List.Icon theme={theme} icon="cloud-off-outline" />
+            <View style={{ marginLeft: 16 }}>
+              <Text
+                style={{
+                  color: theme.onSurface,
+                  fontSize: 16,
+                }}
+              >
+                {getString('moreScreen.downloadOnly')}
+              </Text>
+              <Text style={{ color: theme.onSurfaceVariant }}>
+                {getString('moreScreen.downloadOnlyDesc')}
+              </Text>
             </View>
-            <Switch
-              value={downloadedOnlyMode}
-              onValueChange={enableDownloadedOnlyMode}
-              color={theme.primary}
-              style={{ marginRight: 8 }}
-            />
-          </Pressable>
-          <Pressable
-            android_ripple={{ color: theme.rippleColor }}
-            style={{
-              paddingHorizontal: 16,
-              paddingVertical: 14,
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}
-            onPress={enableIncognitoMode}
-          >
-            <View style={{ flexDirection: 'row' }}>
-              <List.Icon theme={theme} icon="incognito" />
-              <View style={{ marginLeft: 16 }}>
-                <Text
-                  style={{
-                    color: theme.onSurface,
-                    fontSize: 16,
-                  }}
-                >
-                  {getString('moreScreen.incognitoMode')}
-                </Text>
-                <Text style={{ color: theme.onSurfaceVariant }}>
-                  {getString('moreScreen.incognitoModeDesc')}
-                </Text>
-              </View>
+          </View>
+          <Switch
+            value={downloadedOnlyMode}
+            onValueChange={enableDownloadedOnlyMode}
+            color={theme.primary}
+            style={{ marginRight: 8 }}
+          />
+        </Pressable>
+        <Pressable
+          android_ripple={{ color: theme.rippleColor }}
+          style={{
+            paddingHorizontal: 16,
+            paddingVertical: 14,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+          onPress={enableIncognitoMode}
+        >
+          <View style={{ flexDirection: 'row' }}>
+            <List.Icon theme={theme} icon="incognito" />
+            <View style={{ marginLeft: 16 }}>
+              <Text
+                style={{
+                  color: theme.onSurface,
+                  fontSize: 16,
+                }}
+              >
+                {getString('moreScreen.incognitoMode')}
+              </Text>
+              <Text style={{ color: theme.onSurfaceVariant }}>
+                {getString('moreScreen.incognitoModeDesc')}
+              </Text>
             </View>
-            <Switch
-              value={incognitoMode}
-              onValueChange={enableIncognitoMode}
-              color={theme.primary}
-              style={{ marginRight: 8 }}
-            />
-          </Pressable>
-          <List.Divider theme={theme} />
-          <List.Item
-            title={getString('moreScreen.downloadQueue')}
-            description={queue.length > 0 ? queue.length + ' remaining' : ''}
-            icon="progress-download"
-            onPress={() =>
-              navigation.navigate('MoreStack', {
-                screen: 'DownloadQueue',
-              })
-            }
-            theme={theme}
+          </View>
+          <Switch
+            value={incognitoMode}
+            onValueChange={enableIncognitoMode}
+            color={theme.primary}
+            style={{ marginRight: 8 }}
           />
-          <List.Item
-            title={getString('common.downloads')}
-            icon="folder-download"
-            onPress={() =>
-              navigation.navigate('MoreStack', {
-                screen: 'Downloads',
-              })
-            }
-            theme={theme}
-          />
-          <List.Item
-            title={getString('common.categories')}
-            icon="label-outline"
-            onPress={() =>
-              navigation.navigate('MoreStack', {
-                screen: 'Categories',
-              })
-            }
-            theme={theme}
-          />
-          <List.Item
-            title={getString('statsScreen.title')}
-            icon="chart-line"
-            onPress={() =>
-              navigation.navigate('MoreStack', {
-                screen: 'Statistics',
-              })
-            }
-            theme={theme}
-          />
-          <List.Divider theme={theme} />
-          <List.Item
-            title={getString('common.settings')}
-            icon="cog-outline"
-            onPress={() =>
-              navigation.navigate('MoreStack', {
-                screen: 'SettingsStack',
-                params: {
-                  screen: 'Settings',
-                },
-              })
-            }
-            theme={theme}
-          />
-          <List.Item
-            title={getString('common.about')}
-            icon="information-outline"
-            onPress={() =>
-              navigation.navigate('MoreStack', {
-                screen: 'About',
-              })
-            }
-            theme={theme}
-          />
-        </List.Section>
-      </ScrollView>
-    </>
+        </Pressable>
+        <List.Divider theme={theme} />
+        <List.Item
+          title={getString('moreScreen.downloadQueue')}
+          description={queue.length > 0 ? queue.length + ' remaining' : ''}
+          icon="progress-download"
+          onPress={() =>
+            navigation.navigate('MoreStack', {
+              screen: 'DownloadQueue',
+            })
+          }
+          theme={theme}
+        />
+        <List.Item
+          title={getString('common.downloads')}
+          icon="folder-download"
+          onPress={() =>
+            navigation.navigate('MoreStack', {
+              screen: 'Downloads',
+            })
+          }
+          theme={theme}
+        />
+        <List.Item
+          title={getString('common.categories')}
+          icon="label-outline"
+          onPress={() =>
+            navigation.navigate('MoreStack', {
+              screen: 'Categories',
+            })
+          }
+          theme={theme}
+        />
+        <List.Item
+          title={getString('statsScreen.title')}
+          icon="chart-line"
+          onPress={() =>
+            navigation.navigate('MoreStack', {
+              screen: 'Statistics',
+            })
+          }
+          theme={theme}
+        />
+        <List.Divider theme={theme} />
+        <List.Item
+          title={getString('common.settings')}
+          icon="cog-outline"
+          onPress={() =>
+            navigation.navigate('MoreStack', {
+              screen: 'SettingsStack',
+              params: {
+                screen: 'Settings',
+              },
+            })
+          }
+          theme={theme}
+        />
+        <List.Item
+          title={getString('common.about')}
+          icon="information-outline"
+          onPress={() =>
+            navigation.navigate('MoreStack', {
+              screen: 'About',
+            })
+          }
+          theme={theme}
+        />
+      </List.Section>
+    </ScrollView>
   );
 };
 
