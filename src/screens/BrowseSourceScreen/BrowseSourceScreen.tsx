fc925b9d7f0fe9a506dbe24b5043d09f5396a1cc
CD-Z <p_loser_Austria@gmx.at>
2024-03-09 20:06:43 +0100

    tailing loading covers

--- src/screens/BrowseSourceScreen/BrowseSourceScreen.tsx
+++ src/screens/BrowseSourceScreen/BrowseSourceScreen.tsx
@@ -16,7 +16,7 @@ import { getString } from '@strings/translations';
 import { StyleSheet } from 'react-native';
 import { useLibraryNovels } from '@screens/library/hooks/useLibrary';
 import { switchNovelToLibrary } from '@database/queries/NovelQueries';
-import { NovelInfo } from '@database/types';
+import { LibraryNovelInfo, NovelInfo } from '@database/types';
 import SourceScreenSkeletonLoading from '@screens/browse/loadingAnimation/SourceScreenSkeletonLoading';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { BrowseSourceScreenProps } from '@navigators/types';
@@ -121,12 +121,13 @@ const BrowseSourceScreen = ({ route, navigation }: BrowseSourceScreenProps) => {
       ) : (
         <NovelList
           data={novelList}
+          inSource
           renderItem={({ item }) => {
             const inLibrary = novelInLibrary(item.path);
 
             return (
               <NovelCover
-                item={item}
+                item={item as LibraryNovelInfo}
                 theme={theme}
                 libraryStatus={inLibrary}
                 onPress={() => navigateToNovel(item)}
@@ -164,6 +165,7 @@ const BrowseSourceScreen = ({ route, navigation }: BrowseSourceScreenProps) => {
               fetchNextPage();
             }
           }}
+          onEndReachedThreshold={1.5}
           ListFooterComponent={
             (hasNextPage && !searchText) ||
             (hasNextSearchPage && searchText) ? (
