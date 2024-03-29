b59586c042b3456c088ba9b2fd79e9635c5acab7
CD-Z <p_loser_Austria@gmx.at>
2024-03-29 17:22:30 +0100

    added strings

--- strings/types/index.ts
+++ strings/types/index.ts
@@ -350,6 +350,7 @@ export interface StringMap {
   'readerScreen.bottomSheet.fontStyle': 'string';
   'readerScreen.bottomSheet.fullscreen': 'string';
   'readerScreen.bottomSheet.lineHeight': 'string';
+  'readerScreen.bottomSheet.padding': 'string';
   'readerScreen.bottomSheet.readerPages': 'string';
   'readerScreen.bottomSheet.removeExtraSpacing': 'string';
   'readerScreen.bottomSheet.renderHml': 'string';
@@ -358,7 +359,6 @@ export interface StringMap {
   'readerScreen.bottomSheet.showProgressPercentage': 'string';
   'readerScreen.bottomSheet.showSwipeMargins': 'string';
   'readerScreen.bottomSheet.swipeGestures': 'string';
-  'readerScreen.bottomSheet.readerPages': 'string';
   'readerScreen.bottomSheet.textAlign': 'string';
   'readerScreen.bottomSheet.textSize': 'string';
   'readerScreen.bottomSheet.useChapterDrawerSwipeNavigation': 'string';
