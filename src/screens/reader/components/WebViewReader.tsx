c5ed527a0dff0abb1ef6a89f036787a641977456
CD-Z <p_loser_Austria@gmx.at>
2024-03-30 20:59:40 +0100

    removed useless

--- src/screens/reader/components/WebViewReader.tsx
+++ src/screens/reader/components/WebViewReader.tsx
@@ -124,7 +124,7 @@ const WebViewReader: FC<WebViewReaderProps> = props => {
       subscription.remove();
       mmkvListener.remove();
     };
-  });
+  }, []);
   const debugging = `
      // Debug
      console = new Object();
@@ -246,11 +246,6 @@ const WebViewReader: FC<WebViewReaderProps> = props => {
                       --theme-rippleColor: ${theme.rippleColor};
                       --chapterCtn-height: ${layoutHeight - 140};
                       }
-                      .nextButton, .infoText {
-                        margin-left: ${readerSettings.padding}%;
-                        margin-right: ${readerSettings.padding}%;
-                        width: calc(100% - ${readerSettings.padding * 2}%);
-                      } 
                       
                       @font-face {
                         font-family: ${readerSettings.fontFamily};
