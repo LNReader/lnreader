cf6fe551de1758d87f6098c26be44405e7f99ec8
nyagami <hoangquan05112002@gmail.com>
2024-04-30 23:10:03 +0700

    search voice

--- src/screens/settings/SettingsReaderScreen/Modals/VoicePickerModal.tsx
+++ src/screens/settings/SettingsReaderScreen/Modals/VoicePickerModal.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 
-import { Portal, Modal, overlay } from 'react-native-paper';
+import { Portal, Modal, overlay, TextInput } from 'react-native-paper';
 import { RadioButton } from '@components/RadioButton/RadioButton';
 
 import { useChapterReaderSettings, useTheme } from '@hooks/persisted';
@@ -19,6 +19,8 @@ const VoicePickerModal: React.FC<VoicePickerModalProps> = ({
 }) => {
   const theme = useTheme();
   const [voices, setVoices] = useState<Voice[]>([]);
+  const [searchedVoices, setSearchedVoices] = useState<Voice[]>([]);
+  const [searchText, setSearchText] = useState('');
   const { setChapterReaderSettings, tts } = useChapterReaderSettings();
   useEffect(() => {
     getAvailableVoicesAsync().then(res => {
@@ -37,7 +39,27 @@ const VoicePickerModal: React.FC<VoicePickerModalProps> = ({
         ]}
       >
         <FlashList
-          data={voices}
+          ListHeaderComponent={
+            <TextInput
+              mode="outlined"
+              underlineColor={theme.outline}
+              theme={{ colors: { ...theme } }}
+              onChangeText={text => {
+                setSearchText(text);
+                setSearchedVoices(
+                  voices.filter(voice =>
+                    voice.name
+                      .toLocaleLowerCase()
+                      .includes(text.toLocaleLowerCase()),
+                  ),
+                );
+              }}
+              value={searchText}
+              placeholder="Search voice"
+            />
+          }
+          ListHeaderComponentStyle={{ paddingHorizontal: 12 }}
+          data={searchText ? searchedVoices : voices}
           extraData={tts?.voice}
           renderItem={({ item }) => (
             <RadioButton
