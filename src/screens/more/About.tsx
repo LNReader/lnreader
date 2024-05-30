aec6c34b6634e5099d30b917f73e84c6b8d735bc
Justin COLLON <48163201+error7404@users.noreply.github.com>
2024-05-30 19:00:07 +0200

    Update: Version utils & Github actions (#1072)
    * github action:
     - custom apk:
       - packet name with git commit id
       - app name with git commit id
     - cached `npm install`
     - update actions versions
    
    * About page:
     - Version based on package.json
     - added git hash to the version if not Stable
     - RELEASE_DATE, GIT_HASH, BUILD_TYPE created at build time from setEnvFile.cjs
     - special BUILD_TYPE for github actions
     - Version modal copy on click
     - removed versionUtils
    
    * expo-clipboard instead of react-native clipboard
    
    * buildRelease Release instead of Debug ^^'
    Gihtub Actions => Github Action

--- src/screens/more/About.tsx
+++ src/screens/more/About.tsx
@@ -6,13 +6,29 @@ import * as Linking from 'expo-linking';
 import { getString } from '@strings/translations';
 import { ScreenContainer } from '@components/Common';
 import { MoreHeader } from './components/MoreHeader';
-import { appVersion, releaseDate } from '@utils/versionUtils';
 import { useTheme } from '@hooks/persisted';
 import { List } from '@components';
 import { AboutScreenProps } from '@navigators/types';
+import { GIT_HASH, RELEASE_DATE, BUILD_TYPE } from '@env';
+import * as Clipboard from 'expo-clipboard';
+import { version } from '../../../package.json';
 
 const AboutScreen = ({ navigation }: AboutScreenProps) => {
   const theme = useTheme();
+
+  function getBuildName() {
+    if (!GIT_HASH || !RELEASE_DATE || !BUILD_TYPE) {
+      return `Custom build ${version}`;
+    } else {
+      const localDateTime = isNaN(Number(RELEASE_DATE))
+        ? RELEASE_DATE
+        : new Date(Number(RELEASE_DATE)).toLocaleString();
+      if (BUILD_TYPE === 'Release') {
+        return `${BUILD_TYPE} ${version} (${localDateTime})`;
+      }
+      return `${BUILD_TYPE} ${version} (${localDateTime}) Commit: ${GIT_HASH}`;
+    }
+  }
 
   return (
     <ScreenContainer theme={theme}>
@@ -26,14 +42,17 @@ const AboutScreen = ({ navigation }: AboutScreenProps) => {
         <List.Section>
           <List.Item
             title={getString('aboutScreen.version')}
-            description={`Stable ${appVersion} (${releaseDate})`}
+            description={getBuildName()}
             theme={theme}
+            onPress={() => {
+              Clipboard.setStringAsync(getBuildName());
+            }}
           />
           <List.Item
             title={getString('aboutScreen.whatsNew')}
             onPress={() =>
               Linking.openURL(
-                `https://github.com/LNReader/lnreader/releases/tag/v${appVersion}`,
+                `https://github.com/LNReader/lnreader/releases/tag/v${version}`,
               )
             }
             theme={theme}
--- src/utils/versionUtils.ts
+++
@@ -1,2 +0,0 @@
-export const appVersion = '2.0.0';
-export const releaseDate = '01/02/24 09:00 AM';
