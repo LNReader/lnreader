2171493200fca446f000aae7ff19916775ef0f89
nyagami <hoangquan05112002@gmail.com>
2024-05-31 14:42:18 +0700

    pick theme step

--- src/screens/onboarding/OnboardingScreen.tsx
+++ src/screens/onboarding/OnboardingScreen.tsx
@@ -1,7 +1,7 @@
 import { useTheme } from '@hooks/persisted';
 import { Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import { Button } from '@components';
 import PickThemeStep from './PickThemeStep';
 import { useState } from 'react';
@@ -9,13 +9,45 @@ import StorageStep from './StorageStep';
 
 export default function OnboardingScreen() {
   const theme = useTheme();
-  const steps = [PickThemeStep, StorageStep];
   const [step, setStep] = useState(0);
   return (
     <SafeAreaView style={[styles.root, { backgroundColor: theme.background }]}>
-      <Text>Hello!</Text>
-      <Text>Youkoso jitsuryoku shijou shugi no kyoushitsu e</Text>
-      {steps[step]()}
+      <Image
+        source={require('../../../assets/logo.png')}
+        tintColor={theme.onBackground}
+        style={{
+          width: 90,
+          height: 90,
+        }}
+      />
+      <Text
+        variant="headlineLarge"
+        style={{
+          fontWeight: '600',
+          paddingBottom: 8,
+          color: theme.primary,
+        }}
+      >
+        Yaholo!
+      </Text>
+      <Text
+        style={{
+          fontWeight: '500',
+          paddingBottom: 8,
+          color: theme.onBackground,
+        }}
+      >
+        Youkoso jitsuryoku shijou shugi no kyoushitsu e
+      </Text>
+      <View
+        style={[
+          styles.stepContainer,
+          { backgroundColor: theme.primaryContainer },
+        ]}
+      >
+        {step === 0 ? <PickThemeStep /> : <StorageStep />}
+      </View>
+      <View style={{ flex: 1 }} />
       <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
         {step === 0 ? (
           <Button
@@ -42,5 +74,10 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 16,
     paddingTop: 40,
+    paddingBottom: 16,
+  },
+  stepContainer: {
+    paddingVertical: 16,
+    borderRadius: 12,
   },
 });
--- src/screens/onboarding/PickThemeStep.tsx
+++ src/screens/onboarding/PickThemeStep.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 import {
   defaultTheme,
   midnightDusk,
@@ -14,6 +14,7 @@ import { ThemeColors } from '@theme/types';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useMMKVObject } from 'react-native-mmkv';
 import { useTheme } from '@hooks/persisted';
+
 const lightThemes = [
   defaultTheme.light,
   midnightDusk.light,
@@ -23,15 +24,15 @@ const lightThemes = [
   strawberryDaiquiriTheme.light,
   takoTheme.light,
 ];
-// const darkThemes = [
-//     defaultTheme.dark,
-//     midnightDusk.dark,
-//     tealTurquoise.dark,
-//     yotsubaTheme.dark,
-//     lavenderTheme.dark,
-//     strawberryDaiquiriTheme.dark,
-//     takoTheme.dark,
-// ];
+const darkThemes = [
+  defaultTheme.dark,
+  midnightDusk.dark,
+  tealTurquoise.dark,
+  yotsubaTheme.dark,
+  lavenderTheme.dark,
+  strawberryDaiquiriTheme.dark,
+  takoTheme.dark,
+];
 
 const ThemeList = ({
   theme,
@@ -65,15 +66,99 @@ const ThemeList = ({
   );
 };
 
+const SchemeButton = ({
+  onPress,
+  isDarkButton = false,
+}: {
+  onPress: () => void;
+  isDarkButton?: boolean;
+}) => {
+  const theme = useTheme();
+  const isSelected =
+    (theme.isDark && isDarkButton) || (!theme.isDark && !isDarkButton);
+  return (
+    <View
+      style={[
+        styles.schemeButtonWrapper,
+        isDarkButton ? styles.darkButton : styles.lightButton,
+        {
+          borderColor: theme.onSurface,
+          backgroundColor: isSelected ? theme.primary : theme.surface,
+        },
+      ]}
+    >
+      <Pressable
+        android_ripple={{ color: theme.rippleColor, borderless: true }}
+        style={styles.schemeButton}
+        onPress={onPress}
+      >
+        <Text style={{ color: isSelected ? theme.onPrimary : theme.onSurface }}>
+          {isDarkButton ? 'Dark' : 'Light'}
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const getInverseTheme = (theme: ThemeColors) => {
+  let index = (theme.isDark ? darkThemes : lightThemes).findIndex(
+    item => item.id === theme.id,
+  );
+  if (index === -1) {
+    index = 1;
+  }
+  if (theme.isDark) {
+    return lightThemes[index];
+  }
+  return darkThemes[index];
+};
+
 export default function PickThemeStep() {
   const theme = useTheme();
+  const [, setTheme] = useMMKVObject('APP_THEME');
   return (
     <View>
-      <View style={{ flexDirection: 'row' }}>
-        <Text>Light</Text>
-        <Text>Dark</Text>
+      <View style={styles.buttonContainer}>
+        <SchemeButton
+          onPress={() => {
+            setTheme(getInverseTheme(theme));
+          }}
+        />
+        <SchemeButton
+          onPress={() => {
+            setTheme(getInverseTheme(theme));
+          }}
+          isDarkButton={true}
+        />
       </View>
-      <ThemeList theme={theme} list={lightThemes} />
+      <ThemeList theme={theme} list={theme.isDark ? darkThemes : lightThemes} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingBottom: 24,
+  },
+  schemeButtonWrapper: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+  },
+  schemeButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 36,
+  },
+  lightButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderTopLeftRadius: 24,
+    borderBottomLeftRadius: 24,
+  },
+  darkButton: {
+    borderRightWidth: 1,
+    borderTopRightRadius: 24,
+    borderBottomRightRadius: 24,
+  },
+});
