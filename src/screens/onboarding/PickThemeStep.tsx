07597a603aa4612b75698582bb0c6decefcff4c5
nyagami <hoangquan05112002@gmail.com>
2024-06-03 20:53:41 +0700

    refactor: corresponding theme list

--- src/screens/onboarding/PickThemeStep.tsx
+++ src/screens/onboarding/PickThemeStep.tsx
@@ -1,38 +1,11 @@
 import React from 'react';
 import { View, Text, Pressable, StyleSheet } from 'react-native';
-import {
-  defaultTheme,
-  midnightDusk,
-  tealTurquoise,
-  yotsubaTheme,
-  lavenderTheme,
-  strawberryDaiquiriTheme,
-  takoTheme,
-} from '@theme/md3';
 import { ThemePicker } from '@components/ThemePicker/ThemePicker';
 import { ThemeColors } from '@theme/types';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useMMKVObject } from 'react-native-mmkv';
 import { useTheme } from '@hooks/persisted';
-
-const lightThemes = [
-  defaultTheme.light,
-  midnightDusk.light,
-  tealTurquoise.light,
-  yotsubaTheme.light,
-  lavenderTheme.light,
-  strawberryDaiquiriTheme.light,
-  takoTheme.light,
-];
-const darkThemes = [
-  defaultTheme.dark,
-  midnightDusk.dark,
-  tealTurquoise.dark,
-  yotsubaTheme.dark,
-  lavenderTheme.dark,
-  strawberryDaiquiriTheme.dark,
-  takoTheme.dark,
-];
+import { darkThemes, lightThemes } from '@theme/md3';
 
 const ThemeList = ({
   theme,
