eb4f65c17d8d0d59e2a756e579004ebe637b4b0b
Batorian <lukas.richter.berlin@web.de>
2024-06-07 02:30:20 +0200

    Enhancement: added more CSS

--- src/screens/reader/utils/sanitizeChapterText.ts
+++ src/screens/reader/utils/sanitizeChapterText.ts
@@ -9,23 +9,72 @@ export const sanitizeChapterText = (
   html: string,
   options?: Options,
 ): string => {
-  if (html) {
-    html = html.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
-  }
+  const allowedStyles: { [key: string]: string[] } = {
+    'game-prompt-gold-info': ['font-family', 'text-align'],
+    'game-prompt-gold-info-title': ['font-family', 'text-align'],
+    'game-prompt-gold-info-content': ['font-family', 'text-align'],
+    'game-prompt-info': ['font-family', 'text-align'],
+    'game-prompt-info-title': ['font-family', 'text-align'],
+    'game-prompt-info-content': ['font-family', 'text-align'],
+    'game-prompt-info-small': ['font-family', 'text-align'],
+    'game-prompt-info-small-content': ['font-family', 'text-align'],
+    'game-prompt-silver-info': ['font-family', 'text-align'],
+    'game-prompt-silver-info-content': ['font-family', 'text-align'],
+    'game-prompt-warning': ['font-family', 'text-align'],
+    'game-prompt-warning-title': ['font-family', 'text-align'],
+    'game-prompt-warning-content': ['font-family', 'text-align'],
+    'novel-system-box': ['font-family', 'text-align'],
+    'letter-style': ['font-family', 'text-align'],
+    'system': ['font-family', 'text-align'],
+  };
   let text = sanitizeHtml(html, {
     allowedTags: sanitizeHtml.defaults.allowedTags.concat([
-      'img',
-      'i',
-      'em',
-      'b',
       'a',
+      'b',
       'div',
+      'em',
+      'i',
+      'img',
+      'li',
+      'ol',
     ]),
     allowedAttributes: {
-      'img': ['src'],
-      'a': ['href'],
+      'a': ['href', 'class', 'id'],
+      'div': ['class', 'id', 'style'],
+      'img': ['src', 'class', 'id'],
+      'ol': ['reversed', 'start', 'type'],
+      'p': ['class', 'id'],
     },
     allowedSchemes: ['data', 'http', 'https', 'file'],
+    transformTags: {
+      'div': (tagName, attribs) => {
+        if (attribs.class) {
+          const classes = attribs.class.split(' ');
+          for (const cls of classes) {
+            if (allowedStyles[cls]) {
+              const styles = attribs.style ? attribs.style.split(';') : [];
+              const allowedStylesForClass = styles.filter(style => {
+                const [property] = style.split(':');
+                return allowedStyles[cls].includes(property.trim());
+              });
+              return {
+                tagName: tagName,
+                attribs: {
+                  ...attribs,
+                  style: allowedStylesForClass.join(';'),
+                },
+              };
+            }
+          }
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { style, ...rest } = attribs;
+        return {
+          tagName: tagName,
+          attribs: rest,
+        };
+      },
+    },
   });
   if (text) {
     if (options?.removeExtraParagraphSpacing) {
