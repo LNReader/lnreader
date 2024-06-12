b568a565d25e0cb0e024f3441e99c1d250960a87
Patrick Loser <p_loser_austria@gmx.at>
2024-06-12 16:41:33 +0200

    added correct types

--- src/screens/reader/components/ChapterDrawer/RenderListChapter.tsx
+++ src/screens/reader/components/ChapterDrawer/RenderListChapter.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { View, Pressable } from 'react-native';
+import { View, Pressable, TextStyle } from 'react-native';
 import { Text } from 'react-native-paper';
 import color from 'color';
 import { ChapterInfo, NovelInfo } from '@database/types';
 import { ThemeColors } from '@theme/types';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '@navigators/types';
+import { StyleProp } from 'react-native';
+import { ViewStyle } from 'react-native';
 
 type Styles = {
-  chapterCtn: object;
-  drawerElementContainer: object;
-  chapterNameCtn: object;
-  releaseDateCtn: object;
+  chapterCtn: StyleProp<ViewStyle>;
+  drawerElementContainer: StyleProp<ViewStyle>;
+  chapterNameCtn: StyleProp<TextStyle>;
+  releaseDateCtn: StyleProp<TextStyle>;
 };
 type Navigation = StackNavigationProp<RootStackParamList, 'Chapter', undefined>;
 
