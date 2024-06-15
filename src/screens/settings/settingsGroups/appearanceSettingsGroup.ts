ed3a91da2f5d835f2eb2c008994e554cea152cbb
CD-Z <p_loser_Austria@gmx.at>
2024-06-15 19:37:18 +0200

    added Appearance settings themePicker

---
+++ src/screens/settings/settingsGroups/appearanceSettingsGroup.ts
@@ -0,0 +1,27 @@
+import { getString } from '@strings/translations';
+import { SettingsGroup } from '../Settings';
+import { darkThemes, lightThemes } from '@theme/md3';
+
+const AppearanceSettings: SettingsGroup = {
+  groupTitle: getString('appearance'),
+  icon: 'palette-outline',
+  navigateParam: 'AppearanceSettings',
+  subGroup: [
+    {
+      subGroupTitle: getString('appearanceScreen.appTheme'),
+      settings: [
+        {
+          title: getString('appearanceScreen.lightTheme'),
+          type: 'ThemePicker',
+          options: lightThemes,
+        },
+        {
+          title: getString('appearanceScreen.darkTheme'),
+          type: 'ThemePicker',
+          options: darkThemes,
+        },
+      ],
+    },
+  ],
+};
+export default AppearanceSettings;
