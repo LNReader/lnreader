bd4d9ab290bd623674ea805db2b13be8f760f85a
CD-Z <p_loser_Austria@gmx.at>
2024-06-15 21:21:26 +0200

    finished appearance screen

--- src/screens/settings/settingsGroups/appearanceSettingsGroup.ts
+++ src/screens/settings/settingsGroups/appearanceSettingsGroup.ts
@@ -28,6 +28,51 @@ const AppearanceSettings: SettingsGroup = {
         },
       ],
     },
+    {
+      subGroupTitle: getString('appearanceScreen.novelInfo'),
+      settings: [
+        {
+          title: getString('appearanceScreen.hideBackdrop'),
+          type: 'Switch',
+          settingOrigin: 'App',
+          valueKey: 'hideBackdrop',
+          defaultValue: false,
+        },
+        {
+          title: getString('advancedSettingsScreen.useFAB'),
+          type: 'Switch',
+          settingOrigin: 'App',
+          valueKey: 'useFabForContinueReading',
+          defaultValue: false,
+        },
+      ],
+    },
+    {
+      subGroupTitle: getString('appearanceScreen.navbar'),
+      settings: [
+        {
+          title: getString('appearanceScreen.showUpdatesInTheNav'),
+          type: 'Switch',
+          settingOrigin: 'App',
+          valueKey: 'showUpdatesTab',
+          defaultValue: true,
+        },
+        {
+          title: getString('appearanceScreen.showHistoryInTheNav'),
+          type: 'Switch',
+          settingOrigin: 'App',
+          valueKey: 'showHistoryTab',
+          defaultValue: true,
+        },
+        {
+          title: getString('appearanceScreen.alwaysShowNavLabels'),
+          type: 'Switch',
+          settingOrigin: 'App',
+          valueKey: 'showLabelsInNav',
+          defaultValue: true,
+        },
+      ],
+    },
   ],
 };
 export default AppearanceSettings;
