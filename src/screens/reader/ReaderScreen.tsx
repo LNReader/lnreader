d119bb4552811fbab57655b356d2bbb269287197
r1di <33724815+r1di@users.noreply.github.com>
2024-06-23 17:31:53 +0200

    Volume scrolling fix
    the scrolling by volume button right now scrolls 100% of the screen, which hides text when scrolled so, the commit is just changing the the scrolling to 50%.

--- src/screens/reader/ReaderScreen.tsx
+++ src/screens/reader/ReaderScreen.tsx
@@ -132,15 +132,12 @@ export const ChapterContent = ({
     VolumeButtonListener.preventDefault();
     emmiter.current.addListener('VolumeUp', () => {
       webViewRef.current?.injectJavaScript(`(()=>{
-          window.scrollBy({top:${-Dimensions.get('window')
-            .height},behavior:'smooth',})
+          window.scrollBy({top: -${Dimensions.get('window').height / 2}, behavior: 'smooth'})
         })()`);
     });
     emmiter.current.addListener('VolumeDown', () => {
       webViewRef.current?.injectJavaScript(`(()=>{
-          window.scrollBy({top:${
-            Dimensions.get('window').height
-          },behavior:'smooth',})
+          window.scrollBy({top: ${Dimensions.get('window').height / 2}, behavior: 'smooth'})
         })()`);
     });
   };
