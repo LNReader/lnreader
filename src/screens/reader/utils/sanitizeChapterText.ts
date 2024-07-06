afa41e6e78c5151f73a330ee9f8649574b2e65fe
Batorian <lukas.richter.berlin@web.de>
2024-07-06 18:46:51 +0200

    sanitizer: update sanitizer

--- src/screens/reader/utils/sanitizeChapterText.ts
+++ src/screens/reader/utils/sanitizeChapterText.ts
@@ -9,26 +9,8 @@ export const sanitizeChapterText = (
   html: string,
   options?: Options,
 ): string => {
-  // List of allowed CSS properties with regex patterns for margin and padding
-  const allowedCSSProperties: RegExp[] = [
-    /^align-items$/,
-    /^background-color$/,
-    /^border$/,
-    /^border-.+$/,
-    /^color$/,
-    /^display$/,
-    /^justify-content$/,
-    /^margin$/,
-    /^margin-.+$/,
-    /^padding$/,
-    /^padding-.+$/,
-    /^text-align$/,
-    /^text-shadow$/,
-    /^text-transform$/,
-  ];
-
-  // List of tags to apply the style filter to
-  const tagsToFilter = ['a', 'div', 'img', 'ol', 'p'];
+  // List of disallowed CSS properties
+  const disallowedCSSProperties: RegExp[] = [/^font-family$/, /^font-size$/];
 
   // Create a transform function for the specified tags
   const createTransformFunction = () => {
@@ -37,7 +19,7 @@ export const sanitizeChapterText = (
         const styles = attribs.style.split(';');
         const allowedStyles = styles.filter((style: string) => {
           const [property] = style.split(':');
-          return allowedCSSProperties.some(regex =>
+          return !disallowedCSSProperties.some(regex =>
             regex.test(property.trim()),
           );
         });
@@ -58,27 +40,30 @@ export const sanitizeChapterText = (
     };
   };
 
+  // List of allowed tags
+  const allowedTags = sanitizeHtml.defaults.allowedTags.concat([
+    'a',
+    'b',
+    'div',
+    'em',
+    'i',
+    'img',
+    'li',
+    'ol',
+    'p',
+    'span',
+  ]);
+
   // Generate the transformTags object
   const transformTags: {
     [key: string]: (tagName: string, attribs: any) => any;
   } = {};
-  tagsToFilter.forEach(tag => {
+  allowedTags.forEach(tag => {
     transformTags[tag] = createTransformFunction();
   });
 
   let text = sanitizeHtml(html, {
-    allowedTags: sanitizeHtml.defaults.allowedTags.concat([
-      'a',
-      'b',
-      'div',
-      'em',
-      'i',
-      'img',
-      'li',
-      'ol',
-      'p',
-      'span',
-    ]),
+    allowedTags: allowedTags,
     allowedAttributes: {
       'a': ['href', 'class', 'id', 'style'],
       'b': ['class', 'id', 'style'],
