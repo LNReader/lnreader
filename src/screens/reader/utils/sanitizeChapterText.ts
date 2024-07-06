111f7fe0baa2026e2e8961879ce5980765445cfa
Batorian <lukas.richter.berlin@web.de>
2024-07-06 23:40:53 +0200

    sanitizer: updated sanitizer

--- src/screens/reader/utils/sanitizeChapterText.ts
+++ src/screens/reader/utils/sanitizeChapterText.ts
@@ -9,26 +9,88 @@ export const sanitizeChapterText = (
   html: string,
   options?: Options,
 ): string => {
+  // List of disallowed CSS properties
+  const allowedCSSProperties: RegExp[] = [
+    /^align-items$/,
+    /^background-color$/,
+    /^border.*$/,
+    /^display$/,
+    /^font-weight$/,
+    /^justify-content$/,
+    /^margin.*$/,
+    /^padding.*$/,
+    /^position$/,
+    /^text-align$/,
+    /^text-decoration$/,
+    /^text-justify$/,
+    /^text-shadow$/,
+    /^text-transform$/,
+  ];
+
+  // Create a transform function for the specified tags
+  const createTransformFunction = () => {
+    return (tagName: string, attribs: any) => {
+      if (attribs.style) {
+        const styles = attribs.style.split(';');
+        const allowedStyles = styles.filter((style: string) => {
+          const [property] = style.split(':');
+          return allowedCSSProperties.some(regex =>
+            regex.test(property.trim()),
+          );
+        });
+        return {
+          tagName: tagName,
+          attribs: {
+            ...attribs,
+            style: allowedStyles.join(';'),
+          },
+        };
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { style, ...rest } = attribs;
+      return {
+        tagName: tagName,
+        attribs: rest,
+      };
+    };
+  };
+
+  // List of allowed tags
+  const allowedTags = sanitizeHtml.defaults.allowedTags.concat([
+    'a',
+    'b',
+    'div',
+    'em',
+    'i',
+    'img',
+    'li',
+    'ol',
+    'p',
+    'span',
+  ]);
+
+  // Generate the transformTags object
+  const transformTags: {
+    [key: string]: (tagName: string, attribs: any) => any;
+  } = {};
+  allowedTags.forEach(tag => {
+    transformTags[tag] = createTransformFunction();
+  });
+
   let text = sanitizeHtml(html, {
-    allowedTags: sanitizeHtml.defaults.allowedTags.concat([
-      'img',
-      'i',
-      'em',
-      'b',
-      'a',
-      'div',
-      'ol',
-      'li',
-    ]),
+    allowedTags: allowedTags,
     allowedAttributes: {
-      'img': ['src', 'class', 'id'],
-      'a': ['href', 'class', 'id'],
-      'div': ['class', 'id'],
-      'p': ['class', 'id'],
+      'a': ['href', 'class', 'id', 'style'],
+      'div': ['class', 'id', 'style'],
+      'img': ['src', 'class', 'id', 'style'],
       'ol': ['reversed', 'start', 'type'],
+      'p': ['class', 'id', 'style'],
+      'span': ['class', 'id', 'style'],
     },
     allowedSchemes: ['data', 'http', 'https', 'file'],
+    transformTags: transformTags,
   });
+
   if (text) {
     if (options?.removeExtraParagraphSpacing) {
       text = text.replace(/<\s*br[^>]*>/gi, '\n').replace(/\n{2,}/g, '\n\n');
