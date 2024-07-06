d62ddce71dfe57e1780a5e2aa5c56f1c27fb2d63
Batorian <lukas.richter.berlin@web.de>
2024-07-06 23:13:43 +0200

    dev: update sanitizer

--- src/screens/reader/utils/sanitizeChapterText.ts
+++ src/screens/reader/utils/sanitizeChapterText.ts
@@ -9,74 +9,88 @@ export const sanitizeChapterText = (
   html: string,
   options?: Options,
 ): string => {
-  const allowedStyles: { [key: string]: string[] } = {
-    'game-prompt-gold-info': ['font-family', 'text-align'],
-    'game-prompt-gold-info-title': ['font-family', 'text-align'],
-    'game-prompt-gold-info-content': ['font-family', 'text-align'],
-    'game-prompt-info': ['font-family', 'text-align'],
-    'game-prompt-info-title': ['font-family', 'text-align'],
-    'game-prompt-info-content': ['font-family', 'text-align'],
-    'game-prompt-info-small': ['font-family', 'text-align'],
-    'game-prompt-info-small-content': ['font-family', 'text-align'],
-    'game-prompt-silver-info': ['font-family', 'text-align'],
-    'game-prompt-silver-info-content': ['font-family', 'text-align'],
-    'game-prompt-warning': ['font-family', 'text-align'],
-    'game-prompt-warning-title': ['font-family', 'text-align'],
-    'game-prompt-warning-content': ['font-family', 'text-align'],
-    'novel-system-box': ['font-family', 'text-align'],
-    'letter-style': ['font-family', 'text-align'],
-    'status': ['font-family', 'text-align'],
-    'system': ['font-family', 'text-align'],
-  };
-  let text = sanitizeHtml(html, {
-    allowedTags: sanitizeHtml.defaults.allowedTags.concat([
-      'a',
-      'b',
-      'div',
-      'em',
-      'i',
-      'img',
-      'li',
-      'ol',
-    ]),
-    allowedAttributes: {
-      'a': ['href', 'class', 'id'],
-      'div': ['class', 'id', 'style'],
-      'img': ['src', 'class', 'id'],
-      'ol': ['reversed', 'start', 'type'],
-      'p': ['class', 'id'],
-    },
-    allowedSchemes: ['data', 'http', 'https', 'file'],
-    transformTags: {
-      'div': (tagName, attribs) => {
-        if (attribs.class) {
-          const classes = attribs.class.split(' ');
-          for (const cls of classes) {
-            if (allowedStyles[cls]) {
-              const styles = attribs.style ? attribs.style.split(';') : [];
-              const allowedStylesForClass = styles.filter(style => {
-                const [property] = style.split(':');
-                return allowedStyles[cls].includes(property.trim());
-              });
-              return {
-                tagName: tagName,
-                attribs: {
-                  ...attribs,
-                  style: allowedStylesForClass.join(';'),
-                },
-              };
-            }
-          }
-        }
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { style, ...rest } = attribs;
+  // List of disallowed CSS properties
+  const allowedCSSProperties: RegExp[] = [
+    /^align-items$/,
+    /^background-color$/,
+    /^border.*$/,
+    /^display$/,
+    /^font-weight$/,
+    /^justify-content$/,
+    /^margin.*$/,
+    /^padding.*$/,
+    /^position$/,
+    /^text-align$/,
+    /^text-decoration$/,
+    /^text-justify$/,
+    /^text-shadow$/,
+    /^text-transform$/,
+  ];
+
+  // Create a transform function for the specified tags
+  const createTransformFunction = () => {
+    return (tagName: string, attribs: any) => {
+      if (attribs.style) {
+        const styles = attribs.style.split(';');
+        const allowedStyles = styles.filter((style: string) => {
+          const [property] = style.split(':');
+          return allowedCSSProperties.some(regex =>
+            regex.test(property.trim()),
+          );
+        });
         return {
           tagName: tagName,
-          attribs: rest,
+          attribs: {
+            ...attribs,
+            style: allowedStyles.join(';'),
+          },
         };
-      },
-    },
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { style, ...rest } = attribs;
+      return {
+        tagName: tagName,
+        attribs: rest,
+      };
+    };
+  };
+
+  // List of allowed tags
+  const allowedTags = sanitizeHtml.defaults.allowedTags.concat([
+    'a',
+    'b',
+    'div',
+    'em',
+    'i',
+    'img',
+    'li',
+    'ol',
+    'p',
+    'span',
+  ]);
+
+  // Generate the transformTags object
+  const transformTags: {
+    [key: string]: (tagName: string, attribs: any) => any;
+  } = {};
+  allowedTags.forEach(tag => {
+    transformTags[tag] = createTransformFunction();
   });
+
+  let text = sanitizeHtml(html, {
+    allowedTags: allowedTags,
+    allowedAttributes: {
+      'a': ['href', 'class', 'id', 'style'],
+      'div': ['class', 'id', 'style'],
+      'img': ['src', 'class', 'id', 'style'],
+      'ol': ['reversed', 'start', 'type'],
+      'p': ['class', 'id', 'style'],
+      'span': ['class', 'id', 'style'],
+    },
+    allowedSchemes: ['data', 'http', 'https', 'file'],
+    transformTags: transformTags,
+  });
+
   if (text) {
     if (options?.removeExtraParagraphSpacing) {
       text = text.replace(/<\s*br[^>]*>/gi, '\n').replace(/\n{2,}/g, '\n\n');
