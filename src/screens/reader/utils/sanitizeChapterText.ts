d0536050c88ff2354487ad37879707c7db1372f3
Batorian <lukas.richter.berlin@web.de>
2024-07-06 18:22:28 +0200

    sanitizer: update sanitizer

--- src/screens/reader/utils/sanitizeChapterText.ts
+++ src/screens/reader/utils/sanitizeChapterText.ts
@@ -9,26 +9,92 @@ export const sanitizeChapterText = (
   html: string,
   options?: Options,
 ): string => {
+  // List of allowed CSS properties with regex patterns for margin and padding
+  const allowedCSSProperties: RegExp[] = [
+    /^align-items$/,
+    /^background-color$/,
+    /^border$/,
+    /^border-.+$/,
+    /^color$/,
+    /^display$/,
+    /^justify-content$/,
+    /^margin$/,
+    /^margin-.+$/,
+    /^padding$/,
+    /^padding-.+$/,
+    /^text-align$/,
+    /^text-shadow$/,
+    /^text-transform$/,
+  ];
+
+  // List of tags to apply the style filter to
+  const tagsToFilter = ['a', 'div', 'img', 'ol', 'p'];
+
+  // Create a transform function for the specified tags
+  const createTransformFunction = () => {
+    return (tagName: string, attribs: any) => {
+      if (attribs.style) {
+        const styles = attribs.style.split(';');
+        const allowedStyles = styles.filter((style: string) => {
+          const [property] = style.split(':');
+          return allowedCSSProperties.some(regex =>
+            regex.test(property.trim()),
+          );
+        });
+        return {
+          tagName: tagName,
+          attribs: {
+            ...attribs,
+            style: allowedStyles.join(';'),
+          },
+        };
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { style, ...rest } = attribs;
+      return {
+        tagName: tagName,
+        attribs: rest,
+      };
+    };
+  };
+
+  // Generate the transformTags object
+  const transformTags: {
+    [key: string]: (tagName: string, attribs: any) => any;
+  } = {};
+  tagsToFilter.forEach(tag => {
+    transformTags[tag] = createTransformFunction();
+  });
+
   let text = sanitizeHtml(html, {
     allowedTags: sanitizeHtml.defaults.allowedTags.concat([
-      'img',
-      'i',
-      'em',
-      'b',
       'a',
+      'b',
       'div',
-      'ol',
+      'em',
+      'i',
+      'img',
       'li',
+      'ol',
+      'p',
+      'span',
     ]),
     allowedAttributes: {
-      'img': ['src', 'class', 'id'],
-      'a': ['href', 'class', 'id'],
-      'div': ['class', 'id'],
-      'p': ['class', 'id'],
-      'ol': ['reversed', 'start', 'type'],
+      'a': ['href', 'class', 'id', 'style'],
+      'b': ['class', 'id', 'style'],
+      'div': ['class', 'id', 'style'],
+      'em': ['class', 'id', 'style'],
+      'i': ['class', 'id', 'style'],
+      'img': ['src', 'class', 'id', 'style'],
+      'li': ['class', 'id', 'style'],
+      'ol': ['reversed', 'start', 'type', 'style'],
+      'p': ['class', 'id', 'style'],
+      'span': ['class', 'id', 'style'],
     },
     allowedSchemes: ['data', 'http', 'https', 'file'],
+    transformTags: transformTags,
   });
+
   if (text) {
     if (options?.removeExtraParagraphSpacing) {
       text = text.replace(/<\s*br[^>]*>/gi, '\n').replace(/\n{2,}/g, '\n\n');
