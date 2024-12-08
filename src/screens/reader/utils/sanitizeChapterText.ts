633aed8057573863a9fcc68015eea974bfc468db
Batorian <lukas.richter.berlin@web.de>
2024-12-08 19:16:29 +0100

    sanitizer: revert sanitizer

--- src/screens/reader/utils/sanitizeChapterText.ts
+++ src/screens/reader/utils/sanitizeChapterText.ts
@@ -1,6 +1,5 @@
 import { getString } from '@strings/translations';
 import sanitizeHtml from 'sanitize-html';
-import * as cheerio from 'cheerio';
 
 export const sanitizeChapterText = (
   pluginId: string,
@@ -8,31 +7,8 @@ export const sanitizeChapterText = (
   chapterName: string,
   html: string,
 ): string => {
-  // Parse the HTML with Cheerio
-  const chapterCheerio = cheerio.load(html);
-
-  // Remove unwanted elements from the chapter content
-  chapterCheerio('noscript').remove(); // Remove script tags
-
-  // Process images to handle lazy-loading attributes
-  chapterCheerio('img').each((i, el) => {
-    const $el = chapterCheerio(el);
-
-    // Prioritize data-lazy-src or src for the main src attribute
-    const imgSrc = $el.attr('data-lazy-src') || $el.attr('src');
-    if (imgSrc) {
-      $el.attr('src', imgSrc); // Set the src value
-    }
-
-    // Prioritize data-lazy-srcset or srcset for the srcset attribute
-    const imgSrcset = $el.attr('data-lazy-srcset') || $el.attr('srcset');
-    if (imgSrcset) {
-      $el.attr('srcset', imgSrcset); // Set the srcset value
-    }
-  });
-
   // Sanitize the HTML to remove unwanted elements
-  let sanitizedHtml = sanitizeHtml(chapterCheerio.html(), {
+  let sanitizedHtml = sanitizeHtml(html, {
     allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'title']),
     allowedAttributes: {
       a: ['href', 'name', 'target', 'class', 'id'],
