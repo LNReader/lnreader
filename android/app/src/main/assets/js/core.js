946863a0dcd3449677897b57a1f10912a65da1b2
Palloxin <75091899+Palloxin@users.noreply.github.com>
2024-12-09 20:43:56 +0100

    u200b

--- android/app/src/main/assets/js/core.js
+++ android/app/src/main/assets/js/core.js
@@ -466,7 +466,7 @@ window.addEventListener('DOMContentLoaded', async () => {
 
     if (reader.generalSettings.val.removeExtraParagraphSpacing) {
       html = html
-        .replace(/(?:&nbsp;\s*)+(?=<\/?p[> ])/g, '')
+        .replace(/(?:&nbsp;\s*|[\u200b]\s*)+(?=<\/?p[> ])/g, '')
         .replace(/<br>\s*<br>\s*(?:<br>\s*)+/g, '<br><br>') //force max 2 consecutive <br>, chaining regex
         .replace(
           /<br>\s*<br>(?:(?!\s*<(?:em|[iab]|strong|span)[> ])|(?<!(?:\/em|\/[iab]|\/strong|\/span)>\s*<br>\s*<br>))\s*/g,
