9636aab53f9d418f8ad807f753c52131c4f0e1fa
Adam Kemish <49228220+Soopyboo32@users.noreply.github.com>
2025-01-01 19:57:32 +0800

    fix: Resolve Service Manager Issue Caused by Unbound ‘this’ (#1375)
    * Fix servicemanager broken due to unbound this
    
    * Can use  here now!

--- src/services/ServiceManager.ts
+++ src/services/ServiceManager.ts
@@ -119,21 +119,21 @@ export default class ServiceManager {
   async executeTask(task: QueuedBackgroundTask) {
     switch (task.task.name) {
       case 'IMPORT_EPUB':
-        return importEpub(task.task.data, this.setMeta);
+        return importEpub(task.task.data, this.setMeta.bind(this));
       case 'UPDATE_LIBRARY':
-        return updateLibrary(task.task.data || {}, this.setMeta);
+        return updateLibrary(task.task.data || {}, this.setMeta.bind(this));
       case 'DRIVE_BACKUP':
-        return createDriveBackup(task.task.data, this.setMeta);
+        return createDriveBackup(task.task.data, this.setMeta.bind(this));
       case 'DRIVE_RESTORE':
-        return driveRestore(task.task.data, this.setMeta);
+        return driveRestore(task.task.data, this.setMeta.bind(this));
       case 'SELF_HOST_BACKUP':
-        return createSelfHostBackup(task.task.data, this.setMeta);
+        return createSelfHostBackup(task.task.data, this.setMeta.bind(this));
       case 'SELF_HOST_RESTORE':
-        return selfHostRestore(task.task.data, this.setMeta);
+        return selfHostRestore(task.task.data, this.setMeta.bind(this));
       case 'MIGRATE_NOVEL':
-        return migrateNovel(task.task.data, this.setMeta);
+        return migrateNovel(task.task.data, this.setMeta.bind(this));
       case 'DOWNLOAD_CHAPTER':
-        return downloadChapter(task.task.data, this.setMeta);
+        return downloadChapter(task.task.data, this.setMeta.bind(this));
       default:
         return;
     }
