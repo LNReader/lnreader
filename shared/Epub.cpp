631aa0e5ec23913116a710b70f47cd6143d99c81
Palloxin <75091899+Palloxin@users.noreply.github.com>
2025-05-25 22:19:44 +0200

    triple dot

--- shared/Epub.cpp
+++ shared/Epub.cpp
@@ -217,6 +217,8 @@ void clean_summary(std::string &summary)
     summary = std::regex_replace(summary, regx74, "†");
     std::regex regx75("&#135;");
     summary = std::regex_replace(summary, regx75, "‡");
+    std::regex regx76("&hellip;");
+    summary = std::regex_replace(summary, regx76, "…");
     std::regex regx78("&#177;");
     summary = std::regex_replace(summary, regx78, "±");
     std::regex regx79("&#171;");
