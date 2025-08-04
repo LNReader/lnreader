728101d0fc1d4855b017c3f1399dac754f06e4c0
CD-Z <69157453+CD-Z@users.noreply.github.com>
2025-08-04 11:50:53 +0200

    fix modal settings crashing

--- App.tsx
+++ App.tsx
@@ -8,7 +8,6 @@ import { StatusBar, StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import LottieSplashScreen from 'react-native-lottie-splash-screen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Provider as PaperProvider } from 'react-native-paper';
 import * as Notifications from 'expo-notifications';
 
 import { createTables } from '@database/db';
@@ -41,10 +40,8 @@ const App = () => {
     <GestureHandlerRootView style={styles.flex}>
       <AppErrorBoundary>
         <SafeAreaProvider>
-          <PaperProvider>
             <StatusBar translucent={true} backgroundColor="transparent" />
             <Main />
-          </PaperProvider>
         </SafeAreaProvider>
       </AppErrorBoundary>
     </GestureHandlerRootView>
