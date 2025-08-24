daa9c7d04b9fbf09bc5b40294f3f9f01fb8e898f
Luka Mamukashvili <57063715+USLTD@users.noreply.github.com>
2025-08-24 23:28:43 +0400

    Cleanup

--- env.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@env' {
-  export const MYANIMELIST_CLIENT_ID: string;
-  export const ANILIST_CLIENT_ID: string;
-  export const GIT_HASH: string;
-  export const RELEASE_DATE: string;
-  export const BUILD_TYPE: 'Debug' | 'Release' | 'Beta' | 'Github Action';
-}
