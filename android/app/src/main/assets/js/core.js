19057b1f36a2fd27328babbd6cfc0b6c9aa14945
Palloxin <75091899+Palloxin@users.noreply.github.com>
2025-09-06 19:39:47 +0200

    Improve ‘remove extra paragraphs’ feature

--- android/app/src/main/assets/js/core.js
+++ android/app/src/main/assets/js/core.js
@@ -557,12 +557,9 @@ window.addEventListener('load', () => {
     if (reader.generalSettings.val.removeExtraParagraphSpacing) {
       html = html
         .replace(/(?:&nbsp;\s*|[\u200b]\s*)+(?=<\/?p[> ])/g, '')
-        .replace(/<br>\s*<br>\s*(?:<br>\s*)+/g, '<br><br>') //force max 2 consecutive <br>, chaining regex
-        .replace(
-          /<br>\s*<br>(?:(?!\s*<(?:em|[iab]|strong|span)[> ])|(?<!(?:\/em|\/[iab]|\/strong|\/span)>\s*<br>\s*<br>))\s*/g,
-          '',
-        ) //look-around double br. If certain tags aren't near, delete the double br.
-        .replace(/<br>(?:(?=\s*<\/?p[> ])|(?<=<\/?p>\s*<br>))\s*/g, '');
+.replace(/<br>\s*<br>\s*(?:<br>\s*)+/g, '<br><br>') //force max 2 consecutive <br>, chaining regex
+.replace(/<br>\s*<br>[^]+/, _ => `${/\/p>/.test(_)?_.replace(/<br>\s*<br>(?:(?<=<\/?p\b[^>]*><br>\s*<br>)(?=\s*<\/?p[> ]))/g, ''):_}`) //if p exists, delete all double br near p
+.replace(/<br>(?:(?=\s*<\/?p[> ])|(?<=<\/?p>\s*<br>))\s*/g, '');
     }
     reader.chapterElement.innerHTML = html;
   });
